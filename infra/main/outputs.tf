e06250457d14d3dbd307d6d16ebda1e9e2a6dac5
Nikolay Todorov <everflowing555@gmail.com>
2026-08-08 15:56:20 +0300

    feat: add main outputs module

---
+++ infra/main/outputs.tf
@@ -0,0 +1,10 @@
+# output "key_vault_name" {
+#   description = "Key Vault name"
+#   value       = module.key_vault.key_vault_name
+# }
+
+# output "app_insights_connection_string" {
+#   description = "Application Insights connection string"
+#   value       = module.monitoring.app_insights_connection_string
+#   sensitive   = true
+# }
